64862340d0e8b5dd3c28e305c210440169869237
Vinicius <viniholtman28@gmail.com>
2022-12-05 14:48:43 -0300

    Implemented carrousel in home images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
+import { useKeenSlider } from "keen-slider/react";
+
+import {HomeContainer, Product} from "../styles/pages/home"
+
+import 'keen-slider/keen-sldier.min.css'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src="" width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta X</strong>
-          
+          <span>R$ 79,90</span>
         </footer>
       </Product>
     </HomeContainer>
