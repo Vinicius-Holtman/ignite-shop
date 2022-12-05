35336c5dc269a281fd54be655fac018825c7f5bb
Vinicius <viniholtman28@gmail.com>
2022-12-05 13:08:37 -0300

    Created styles to header application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from "../styles/globals";
+import logoImg from "../assets/logo.svg"
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+
+  ) 
 }
